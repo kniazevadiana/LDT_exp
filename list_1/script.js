b9705096c78f9b9c112b6a7d9ca17f87a8ca406d
kniazevadiana <109091867+kniazevadiana@users.noreply.github.com>
2026-09-16 21:24:32 +0300

    убираем no cors (вроде это все равно не работает / не требуется).
    изменения в финальном кусочке только

--- list_1/script.js
+++ list_1/script.js
@@ -3396,11 +3396,13 @@ study.on('end', () => {
       'https://script.google.com/macros/s/AKfycbwseWJo402kIqqKSqUkAvAkeZsVUJz_wNUjrwazhpLol0FM3Kdonh2zDaXFWErRfMjthA/exec?key=en-regardant-vers-le-pays-de-france&study=fin_letter_morph_list_1',
       {
         method: 'POST',
-        mode: 'no-cors',
         headers: { 'Content-Type': 'text/plain;charset=utf-8' },
         body: csv
       }
     )
+      .then(r => r.text())
+      .then(text => console.log('Ответ сервера:', text))
+      .catch(e => console.error('Ошибка отправки:', e))
   } catch (e) {
     console.error('Не удалось отправить данные на сервер:', e)
   }
