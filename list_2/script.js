7be8aa567dbdd44b0314606c3f50df1486b07ade
kniazevadiana <109091867+kniazevadiana@users.noreply.github.com>
2026-09-16 22:47:02 +0300

    создаем кастомное название файла с данными

--- list_2/script.js
+++ list_2/script.js
@@ -3197,8 +3197,18 @@ const study = lab.util.fromObject({
 study.on('end', () => {
   try {
     const csv = study.options.datastore.exportCsv()
+
+    // достаём значения, введённые участником в анкете
+    const state = study.options.datastore.state
+    const pid = state.participant_id || 'unknown'
+    const list = state.exp_list || 'unknown'
+    const sex = state.sex || 'unknown'
+    const stamp = new Date().toISOString().replace(/[:.]/g, '-')
+    const filename = `${pid}_${list}_${sex}_${stamp}.csv`
+
     fetch(
-      'https://script.google.com/macros/s/AKfycbwseWJo402kIqqKSqUkAvAkeZsVUJz_wNUjrwazhpLol0FM3Kdonh2zDaXFWErRfMjthA/exec?key=en-regardant-vers-le-pays-de-france&study=fin_letter_morph_list_1',
+      'https://script.google.com/macros/s/AKfycbwseWJo402kIqqKSqUkAvAkeZsVUJz_wNUjrwazhpLol0FM3Kdonh2zDaXFWErRfMjthA/exec?key=en-regardant-vers-le-pays-de-france&study=fin_letter_morph_list_1' +
+      '&filename=' + encodeURIComponent(filename),
       {
         method: 'POST',
         headers: { 'Content-Type': 'text/plain;charset=utf-8' },
